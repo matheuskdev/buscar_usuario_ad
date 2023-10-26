82f52b8752ef1ec14ae5d2ddcb3a7a86a1eff037
Matheus Guilherme <109048357+matheuskdev@users.noreply.github.com>
2023-10-26 12:14:58 -0300

    Update QueryAD2.ps1
    alteração de StreetAddress para Office

--- QueryAD2.ps1
+++ QueryAD2.ps1
@@ -1,10 +1,11 @@
 # Importa o módulo Active Directory
 Import-Module ActiveDirectory
 
-# Obtém informações dos usuários do Active Directory, incluindo as propriedades desejadas e ordena por Name
-$usuarios = Get-ADUser -Filter * -Properties Name, StreetAddress, Mail, Title, Department | Sort-Object Name
+# Obtém informações dos usuários do Active Directory, incluindo as propriedades desejadas e ordena por SamAccountName
+$usuarios = Get-ADUser -Filter * -Properties Name, Office, Mail, Title, Department | Sort-Object Name
 
 # Exporta os resultados para um arquivo CSV
-$usuarios | Select-Object Name, StreetAddress, Mail, Title, Department | Export-Csv -Path "mk_query-lista2.csv" -NoTypeInformation -Encoding UTF8
+$usuarios | Select-Object Name, Office, Mail, Title, Department | Export-Csv -Path "mk_query-lista.csv" -NoTypeInformation -Encoding UTF8
 
-Write-Host "Relatório CSV gerado em: mk_queryAD.csv"
+Write-Host "Relatório CSV gerado em: mk_query-lista.csv"
+
